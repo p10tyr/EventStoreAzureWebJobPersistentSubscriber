58a77f7f86eada9bffc3c26d165362c1b45c5f2b
Piotr Kula <piotr.kula@kindertons.co.uk>
2018-06-20 12:50:46 +0100

    Example added Please enter the commit message for your changes. Lines starting

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace EventStorePersistentSubscriber
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard //I disabled this
+        // and AzureWebJobsStorage //for saving console/logging output
+        static void Main()
+        {
+            ServicePointManager.DefaultConnectionLimit = Int32.MaxValue;
+
+            using (var loggerFactory = new LoggerFactory())
+            {
+                var config = new JobHostConfiguration();
+                var instrumentationKey =
+                    ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                config.DashboardConnectionString = "";
+                config.LoggerFactory = loggerFactory
+                    .AddApplicationInsights(instrumentationKey, null)
+                    .AddConsole();
+
+
+                Console.WriteLine($"Before Host config");
+
+                var host = new JobHost(config);
+                host.Call(typeof(Functions).GetMethod("ProcessMethod")); //thread blocked in here- if it falls over want azure to report failure/ restart
+                //host.RunAndBlock(); //will block thread and wait for events, we don't not interested in events or schedules
+            }
+        }
+    }
+}
